f2263d1434ef7ae9fa9c472b7d6e378cda3c232c
computer-mann <hpsn16@gmail.com>
2023-08-20 22:38:28 +0000

    Added the elastic service class

---
+++ ElasticWeb/Services/ElasticSearchService.cs
@@ -0,0 +1,25 @@
+﻿using Nest;
+
+namespace ElasticWeb.Services
+{
+    public class ElasticSearchService<T>:IElasticSearch<T> where T : class
+    {
+        private readonly string _index;
+        private readonly IElasticClient
+
+        public ElasticSearchService(string index)
+        {
+            _index = index;
+        }
+
+        public T GetItem(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IElasticSearch<T> where T : class
+    {
+        public T GetItem(string key);
+    }
+}
